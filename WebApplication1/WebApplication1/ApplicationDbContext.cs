115566def72e06bba0dde401894c3d8d7be92e00
jakshayraj <74650745+jakshayraj@users.noreply.github.com>
2021-03-16 09:52:27 +0530

    Commit

---
+++ WebApplication1/WebApplication1/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<AppUser> AppUser { get; set; }
+    }
+}
